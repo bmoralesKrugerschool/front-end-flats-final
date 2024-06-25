91c8b7565b860cc19e96bed4799a41b7290c889d
Suu <sreyes@krugerschool.edu.ec>
2024-06-24 19:40:04 -0500

    Changed names at routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>HOLA</h1>} /> //Página de inicio
+        <Route path="/" element={<h1>Home</h1>} /> //Página de inicio
         <Route path="/login" element={<Login />} /> //Inicio de sesión
         <Route path="/register" element={<Register />} /> //Registro de usuario
-        <Route path="/profile" element={<h1>HOLA</h1>} /> //Perfil del usuario
-        <Route path="/about" element={<h1>HOLA</h1>} /> //Acerca de
-        <Route path="/flats" element={<h1>HOLA</h1>} /> //Listado de pisos
+        <Route path="/profile" element={<h1>Profile</h1>} /> //Perfil del usuario
+        <Route path="/about" element={<h1>About</h1>} /> //Acerca de
+        <Route path="/flats" element={<h1>Flats</h1>} /> //Listado de pisos
         <Route path="/flats/:id" element={<h1>HOLA</h1>} />  //Detalle de un piso
         <Route path="/flats/:id/edit" element={<h1>HOLA</h1>} />   //Edición de un piso
         <Route path="/flats/new" element={<h1>HOLA</h1>} />     //Creación de un piso
-        <Route path="*" element={<h1>HOLA</h1>} />   //Página no encontrada
+        <Route path="*" element={<h1>NotFound</h1>} />   //Página no encontrada
       </Routes>
     </BrowserRouter>
   )
